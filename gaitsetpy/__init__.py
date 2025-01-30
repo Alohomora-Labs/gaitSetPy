8c16e8f45d09c391297ee6ba15819224f8e17458
Harshit Agarwal <aharshit123456@gmail.com>
2025-01-30 22:54:00 +0530

    docs webpage branch

--- gaitsetpy/__init__.py
+++
@@ -1,5 +0,0 @@
-from .dataset import *
-from .eda import *
-from .preprocessing import *
-from .features import *
-from .classification import *
--- gaitsetpy/features/__init__.py
+++
@@ -1,9 +0,0 @@
-"""
-feature_extraction: Extracts gait-specific features.
-
-Features:
-- Stride length calculation
-- Heel strike event mapping
-"""
-
-from .gait_features import calculate_stride_length, map_heel_strikes
