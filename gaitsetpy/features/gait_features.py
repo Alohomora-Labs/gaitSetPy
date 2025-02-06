8ad45fd7e8c1356045fa5a560d7375bb3fed429d
Harshit Agarwal <aharshit123456@gmail.com>
2025-02-06 19:22:41 +0530

    gait features fixed (2)

--- gaitsetpy/features/gait_features.py
+++ gaitsetpy/features/gait_features.py
@@ -1,5 +1,6 @@
 '''
-
+Extraction of Gait Features 
+Maintainer: @aharshit123456
 '''
 
 from .utils import (
@@ -42,7 +43,7 @@ def get_stride_times_for_windows(windows, fs):
     """
     stride_times_array = []
     for window in windows:
-        stride_time = calculate_stride_times(window.values, fs)
+        stride_time = calculate_stride_times(window, fs)
         stride_times_array.append(stride_time)
     return stride_times_array
 
@@ -56,7 +57,7 @@ def get_zero_crossing_rates_for_windows(windows):
     """
     zcr_array = []
     for window in windows:
-        zcr = calculate_zero_crossing_rate(window.values)
+        zcr = calculate_zero_crossing_rate(window)
         zcr_array.append(zcr)
     return zcr_array
 
@@ -71,7 +72,7 @@ def get_freezing_indices_for_windows(windows, fs):
     """
     freezing_indices = []
     for window in windows:
-        freezing_index = calculate_freezing_index(window.values, fs)
+        freezing_index = calculate_freezing_index(window, fs)
         freezing_indices.append(freezing_index)
     return freezing_indices
 
@@ -85,7 +86,7 @@ def get_standard_deviations_for_windows(windows):
     """
     std_devs = []
     for window in windows:
-        std_dev = calculate_standard_deviation(window.values)
+        std_dev = calculate_standard_deviation(window)
         std_devs.append(std_dev)
     return std_devs
 
@@ -99,7 +100,7 @@ def get_entropies_for_windows(windows):
     """
     entropy_array = []
     for window in windows:
-        entropy_value = calculate_entropy(window.values)
+        entropy_value = calculate_entropy(window)
         entropy_array.append(entropy_value)
     return entropy_array
 
@@ -113,7 +114,7 @@ def get_energies_for_windows(windows):
     """
     energy_array = []
     for window in windows:
-        energy = calculate_energy(window.values)
+        energy = calculate_energy(window)
         energy_array.append(energy)
     return energy_array
 
@@ -127,7 +128,7 @@ def get_variances_for_windows(windows):
     """
     variance_array = []
     for window in windows:
-        variance = calculate_variance(window.values)
+        variance = calculate_variance(window)
         variance_array.append(variance)
     return variance_array
 
@@ -141,7 +142,7 @@ def get_kurtosis_for_windows(windows):
     """
     kurtosis_array = []
     for window in windows:
-        kurtosis_value = calculate_kurtosis(window.values)
+        kurtosis_value = calculate_kurtosis(window)
         kurtosis_array.append(kurtosis_value)
     return kurtosis_array
 
@@ -156,81 +157,81 @@ def get_step_times_for_windows(windows, fs):
     """
     step_times = []
     for window in windows:
-        step_time = calculate_step_time(window.values, fs)
+        step_time = calculate_step_time(window, fs)
         step_times.append(step_time)
     return step_times
 
 def get_mean_for_windows(windows):
-    return [calculate_mean(window.values) for window in windows]
+    return [calculate_mean(window) for window in windows]
 
 def get_standard_deviation_for_windows(windows):
-    return [calculate_standard_deviation(window.values) for window in windows]
+    return [calculate_standard_deviation(window) for window in windows]
 
 def get_variance_for_windows(windows):
-    return [calculate_variance(window.values) for window in windows]
+    return [calculate_variance(window) for window in windows]
 
 def get_skewness_for_windows(windows):
-    return [calculate_skewness(window.values) for window in windows]
+    return [calculate_skewness(window) for window in windows]
 
 def get_kurtosis_for_windows(windows):
-    return [calculate_kurtosis(window.values) for window in windows]
+    return [calculate_kurtosis(window) for window in windows]
 
 def get_root_mean_square_for_windows(windows):
-    return [calculate_root_mean_square(window.values) for window in windows]
+    return [calculate_root_mean_square(window) for window in windows]
 
 def get_range_for_windows(windows):
-    return [calculate_range(window.values) for window in windows]
+    return [calculate_range(window) for window in windows]
 
 def get_median_for_windows(windows):
-    return [calculate_median(window.values) for window in windows]
+    return [calculate_median(window) for window in windows]
 
 def get_mode_for_windows(windows):
-    return [calculate_mode(window.values) for window in windows]
+    return [calculate_mode(window) for window in windows]
 
 def get_mean_absolute_value_for_windows(windows):
-    return [calculate_mean_absolute_value(window.values) for window in windows]
+    return [calculate_mean_absolute_value(window) for window in windows]
 
 def get_median_absolute_deviation_for_windows(windows):
-    return [calculate_median_absolute_deviation(window.values) for window in windows]
+    return [calculate_median_absolute_deviation(window) for window in windows]
 
 def get_peak_height_for_windows(windows):
-    return [calculate_peak_height(window.values) for window in windows]
+    return [calculate_peak_height(window) for window in windows]
 
 def get_stride_times_for_windows(windows, fs):
-    return [calculate_stride_times(window.values, fs) for window in windows]
+    return [calculate_stride_times(window, fs) for window in windows]
 
 def get_step_times_for_windows(windows, fs):
-    return [calculate_step_time(window.values, fs) for window in windows]
+    return [calculate_step_time(window, fs) for window in windows]
 
 def get_cadence_for_windows(windows, fs):
-    return [calculate_cadence(window.values, fs) for window in windows]
+    return [calculate_cadence(window, fs) for window in windows]
 
 def get_freezing_index_for_windows(windows, fs):
-    return [calculate_freezing_index(window.values, fs) for window in windows]
+    return [calculate_freezing_index(window, fs) for window in windows]
 
 def get_dominant_frequency_for_windows(windows, fs):
-    return [calculate_dominant_frequency(window.values, fs) for window in windows]
+    return [calculate_dominant_frequency(window, fs) for window in windows]
 
 def get_peak_frequency_for_windows(windows, fs):
-    return [calculate_peak_frequency(window.values, fs) for window in windows]
+    return [calculate_peak_frequency(window, fs) for window in windows]
 
 def get_power_spectral_entropy_for_windows(windows, fs):
-    return [calculate_power_spectral_entropy(window.values, fs) for window in windows]
+    return [calculate_power_spectral_entropy(window, fs) for window in windows]
 
 def get_principal_harmonic_frequency_for_windows(windows, fs):
-    return [calculate_principal_harmonic_frequency(window.values, fs) for window in windows]
+    return [calculate_principal_harmonic_frequency(window, fs) for window in windows]
 
 def get_entropy_for_windows(windows):
-    return [calculate_entropy(window.values) for window in windows]
+    return [calculate_entropy(window) for window in windows]
 
 def get_interquartile_range_for_windows(windows):
-    return [calculate_interquartile_range(window.values) for window in windows]
+    return [calculate_interquartile_range(window) for window in windows]
 
 def get_correlation_for_windows(windows):
-    return [calculate_correlation(window.values[:-1], window.values[1:]) for window in windows]
+    return [calculate_correlation(window[:-1], window[1:]) for window in windows]
 
 def get_auto_regression_coefficients_for_windows(windows, order=3):
-    return [calculate_auto_regression_coefficients(window.values, order) for window in windows]
+    return [calculate_auto_regression_coefficients(window, order) for window in windows]
 
 
 def extract_gait_features(daphnet_windows, fs, time_domain=True, frequency_domain=True, statistical=True):
@@ -256,58 +257,52 @@ def extract_gait_features(daphnet_windows, fs, time_domain=True, frequency_domai
 
     for i, sensor_dict in enumerate(daphnet_windows[:-1]):  # Iterate over 11 sensor dictionaries
         sensor_name = sensor_dict["name"]
-        sensor_windows = sensor_dict["data"]  # List of sliding window DataFrames
+        sensor_windows = sensor_dict["data"]  # List of Pandas Series (sliding windows)
+
+        # Convert all windows to NumPy arrays at once
+        windows_array = [window.to_numpy() for window in sensor_windows]
 
         sensor_features = {
             "name": sensor_name,
-            "features": [],
-            "annotations": []  # Store aligned annotations
+            "features": {},
+            "annotations": annotations  # Assigning full annotation list
         }
 
-        # Iterate over each sliding window
-        for j, window in enumerate(sensor_windows):
-            window_features = {}
+        if time_domain:
+            sensor_features["features"].update({
+                "mean": get_mean_for_windows(windows_array),
+                "std_dev": get_standard_deviation_for_windows(windows_array),
+                "variance": get_variance_for_windows(windows_array),
+                "skewness": get_skewness_for_windows(windows_array),
+                "kurtosis": get_kurtosis_for_windows(windows_array),
+                "rms": get_root_mean_square_for_windows(windows_array),
+                "range": get_range_for_windows(windows_array),
+                "median": get_median_for_windows(windows_array),
+                "mode": get_mode_for_windows(windows_array),
+                "mav": get_mean_absolute_value_for_windows(windows_array),
+                "mad": get_median_absolute_deviation_for_windows(windows_array),
+                "peak_height": get_peak_height_for_windows(windows_array),
+                "stride_time": get_stride_times_for_windows(windows_array, fs),
+                "step_time": get_step_times_for_windows(windows_array, fs),
+                "cadence": get_cadence_for_windows(windows_array, fs)
+            })
 
-            if time_domain:
-                window_features.update({
-                    "mean": get_mean_for_windows(window),
-                    "std_dev": get_standard_deviation_for_windows(window),
-                    "variance": get_variance_for_windows(window),
-                    "skewness": get_skewness_for_windows(window),
-                    "kurtosis": get_kurtosis_for_windows(window),
-                    "rms": get_root_mean_square_for_windows(window),
-                    "range": get_range_for_windows(window),
-                    "median": get_median_for_windows(window),
-                    "mode": get_mode_for_windows(window),
-                    "mav": get_mean_absolute_value_for_windows(window),
-                    "mad": get_median_absolute_deviation_for_windows(window),
-                    "peak_height": get_peak_height_for_windows(window),
-                    "stride_time": get_stride_times_for_windows(window, fs),
-                    "step_time": get_step_times_for_windows(window, fs),
-                    "cadence": get_cadence_for_windows(window, fs)
-                })
+        if frequency_domain:
+            sensor_features["features"].update({
+                "freezing_index": get_freezing_index_for_windows(windows_array, fs),
+                "dominant_frequency": get_dominant_frequency_for_windows(windows_array, fs),
+                "peak_frequency": get_peak_frequency_for_windows(windows_array, fs),
+                "power_spectral_entropy": get_power_spectral_entropy_for_windows(windows_array, fs),
+                "principal_harmonic_frequency": get_principal_harmonic_frequency_for_windows(windows_array, fs)
+            })
 
-            if frequency_domain:
-                window_features.update({
-                    "freezing_index": get_freezing_index_for_windows(window, fs),
-                    "dominant_frequency": get_dominant_frequency_for_windows(window, fs),
-                    "peak_frequency": get_peak_frequency_for_windows(window, fs),
-                    "power_spectral_entropy": get_power_spectral_entropy_for_windows(window, fs),
-                    "principal_harmonic_frequency": get_principal_harmonic_frequency_for_windows(window, fs)
-                })
-
-            if statistical:
-                window_features.update({
-                    "entropy": get_entropy_for_windows(window),
-                    "iqr": get_interquartile_range_for_windows(window),
-                    "correlation": get_correlation_for_windows(window),
-                    "auto_regression_coefficients": get_auto_regression_coefficients_for_windows(window)
-                })
-
-            # Store computed features
-            sensor_features["features"].append(window_features)
-            # Store corresponding annotation
-            sensor_features["annotations"].append(annotations[j])  # Aligning annotation with window
+        if statistical:
+            sensor_features["features"].update({
+                "entropy": get_entropy_for_windows(windows_array),
+                "iqr": get_interquartile_range_for_windows(windows_array),
+                "correlation": get_correlation_for_windows(windows_array),
+                "auto_regression_coefficients": get_auto_regression_coefficients_for_windows(windows_array)
+            })
 
         features.append(sensor_features)
 
