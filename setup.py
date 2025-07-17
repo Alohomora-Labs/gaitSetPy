785f0c0b2dfacab37b832f56c47d0c1f65edebe6
Harshit Agarwal <aharshit123456@gmail.com>
2025-07-17 15:27:15 +0530

    Update version number to 0.1.3 in setup.py

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setup(
     name='gaitsetpy',
-    version='0.1.2',
+    version='0.1.3',
     packages=find_packages(include=["gaitsetpy", "gaitsetpy.*"]),
     description="A Python package for gait analysis using sensor data.",
     long_description=long_description,
